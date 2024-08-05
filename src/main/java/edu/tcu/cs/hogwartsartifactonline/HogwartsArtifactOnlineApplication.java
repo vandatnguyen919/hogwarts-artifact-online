dbc9e9849ee659987d64354e51bfd697bcb58f0c
vandatnguyen919 <vandatnguyen919@gmail.com>
2024-08-06 00:49:28 +0700

    first commit

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactonline/HogwartsArtifactOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsArtifactOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwartsartifactonline/HogwartsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
